106ed26d177daca599d0128d2e9bf2c60a8c0645
Patryk Laskowski <patrick_laskowski@hotmail.com>
2022-10-15 18:47:16 +0200

    Install Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:my_shopping_list/app/features/home/home_page.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
